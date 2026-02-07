674d2585362088c96c7c97f441611a25fc3f44a5
cppcho <cppcho@users.noreply.github.com>
2026-02-07 15:31:04 +0900

    Add GBrowse

--- nvim/.config/nvim/lua/plugins/tpope.lua
+++ nvim/.config/nvim/lua/plugins/tpope.lua
@@ -4,9 +4,9 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
     keys = {
-      { "<leader>ga", "<cmd>Gwrite<CR>",      desc = "Git add (stage file)" },
+      { "<leader>ga", "<cmd>Gwrite<CR>", desc = "Git add (stage file)" },
       { "<leader>gs", "<cmd>topleft Git<CR>", desc = "Git status" },
-      { "<leader>gd", "<cmd>Gdiffsplit<CR>",  desc = "Git diff" },
+      { "<leader>gd", "<cmd>Gdiffsplit<CR>", desc = "Git diff" },
     },
     config = function()
       vim.api.nvim_create_autocmd("User", {
@@ -16,5 +16,12 @@ return {
         end,
       })
     end,
+  },
+  -- https://github.com/tpope/vim-rhubarb
+  {
+    "tpope/vim-rhubarb",
+    keys = {
+      { "<leader>gB", "<cmd>GBrowse<CR>", desc = "[B]rowse in GitHub" },
+    },
   },
 }
