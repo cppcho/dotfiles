7177a423e4e671e9b364dfd43172399517411655
cppcho <cppcho@users.noreply.github.com>
2026-02-08 21:38:57 +0900

    make neovide more like macvim

--- nvim/.config/nvim/lua/config/gui.lua
+++ nvim/.config/nvim/lua/config/gui.lua
@@ -4,10 +4,17 @@ if vim.g.neovide then
   vim.o.wrap = true
   vim.o.linebreak = true
 
-  -- Allow clipboard copy/paste with Cmd+C/V
   vim.keymap.set("n", "<D-v>", '"+P', { desc = "Paste from clipboard" })
   vim.keymap.set("v", "<D-v>", '"+P', { desc = "Paste from clipboard" })
   vim.keymap.set("c", "<D-v>", "<C-R>+", { desc = "Paste from clipboard" })
-  vim.keymap.set("i", "<D-v>", '<C-R>+', { desc = "Paste from clipboard" })
+  vim.keymap.set("i", "<D-v>", "<C-R>+", { desc = "Paste from clipboard" })
   vim.keymap.set("t", "<D-v>", '<C-\\><C-n>"+Pi', { desc = "Paste from clipboard" })
+  vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy to clipboard" })
+  vim.keymap.set("v", "<D-x>", '"+d', { desc = "Cut to clipboard" })
+  vim.keymap.set("n", "<D-s>", "<Cmd>w<CR>", { desc = "Save file" })
+  vim.keymap.set("n", "<D-a>", "ggVG", { desc = "Select all" })
+  vim.keymap.set("n", "<D-z>", "u", { desc = "Undo" })
+  vim.keymap.set("n", "<D-Z>", "<C-r>", { desc = "Redo" })
+  vim.keymap.set("n", "<D-t>", "<Cmd>tabnew<CR>", { desc = "New tab" })
+  vim.keymap.set("n", "<D-w>", "<Cmd>tabclose<CR>", { desc = "Close tab" })
 end
