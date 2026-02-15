42c533237d30454ffacefb129613bc9b28326a9c
cppcho <cppcho@users.noreply.github.com>
2026-02-15 11:10:40 +0900

    small changes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,41 @@
--- https://github.com/nvim-treesitter/nvim-treesitter
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
-    local filetypes = {
-      "bash",
-      "c",
-      "css",
-      "diff",
-      "go",
-      "gomod",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "luadoc",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    }
-    require("nvim-treesitter").install(filetypes)
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = filetypes,
-      callback = function()
-        vim.treesitter.start()
-      end,
-    })
-  end,
+  -- https://github.com/nvim-treesitter/nvim-treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
+      local filetypes = {
+        "bash",
+        "c",
+        "css",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      }
+      require("nvim-treesitter").install(filetypes)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = filetypes,
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
+    end,
+  },
+  -- https://github.com/nvim-treesitter/nvim-treesitter-context
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
 }
