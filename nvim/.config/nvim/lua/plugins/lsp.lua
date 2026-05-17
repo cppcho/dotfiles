a2853954793913f646571f904ca90659f12a3c34
cppcho <cppcho@users.noreply.github.com>
2026-05-17 11:56:39 +0900

    refactor: align lsp keymaps with nvim 0.11 gr* convention

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -34,23 +34,5 @@ return {
       "mason-org/mason.nvim",
       "mason-org/mason-lspconfig.nvim",
     },
-    config = function()
-      -- LSP key mappings
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("my-lsp-attach", { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or "n"
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
-
-          -- Rename the variable under your cursor.
-          map("gR", vim.lsp.buf.rename, "[R]e[n]ame")
-
-          -- Execute a code action, usually your cursor needs to be on top of an error
-          map("gA", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-        end,
-      })
-    end,
   },
 }
